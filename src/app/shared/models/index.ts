0feb193d5caaaad8c5fef70547ab784012850c69
Enzo H. De la Sota <lhuaricapcha@gmail.com>
2021-07-21 16:02:50 -0500

    Refac: eliminar models innecesarios

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,5 +1,3 @@
-export * from './infoletGlobal.model';
-export * from './infoletOrigen.model';
 export * from './carga.model';
 export * from './filtros.model';
 export * from './columna.model';
--- src/app/shared/models/infoletGlobal.model.ts
+++
@@ -1,9 +0,0 @@
-export interface InfoletGlobal {
-  archivosProcesados: number;
-  transaccionesCargadas: number;
-  asientosAccountingHub: number;
-  asientosGeneralAccounting: number;
-  movimientoNetoHoy: number;
-  movimientoNetoAyer: number;
-  variacion: number;
-}
