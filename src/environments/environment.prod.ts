b27c201e0ab896343f7ef41532e7917312d2e7e9
Enzo H. De la Sota <lhuaricapcha@gmail.com>
2021-07-23 18:34:24 -0500

    CI/CD: Agregar interceptors a prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import { devBackendProvider } from "src/app/core/interceptors/dev-backend.interceptor";
+
 export const environment = {
   production: true
 };
+
+export const INTERCEPTORS = [
+  devBackendProvider,
+];
