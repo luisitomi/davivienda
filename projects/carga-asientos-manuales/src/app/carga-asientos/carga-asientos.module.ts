308d39da6282113a9c684d0dfccb9b33c57772b6
luis.larosa <luis.larosa@hackmonkeys.com>
2021-12-28 18:54:37 -0500

    feat(main): cambios para borrar material module

--- projects/carga-asientos-manuales/src/app/carga-asientos/carga-asientos.module.ts
+++ projects/carga-asientos-manuales/src/app/carga-asientos/carga-asientos.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CargaAsientosRoutingModule } from './carga-asientos-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../shared/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CargaAsientosManualComponent } from './pages/carga-asientos-manual/carga-asientos-manual.component';
 import { NgxMatFileInputModule } from '@angular-material-components/file-input';
 import { SharedModule } from '../shared';
-import { CombinacionContableComponent } from './components/combinacion-contable/combinacion-contable.component';
 import { cargaAsientosInterceptorProvider } from './interceptors/carga-asientos.interceptor';
 import { NewManualModule } from './pages/nuevo-asiento-manual/nuevo-asiento-manual.module';
 import { ReferenciasComplementariasModule } from './pages/referencias-complementarias/referencias-complementarias.module';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   declarations: [
@@ -22,12 +21,12 @@ import { ReferenciasComplementariasModule } from './pages/referencias-complement
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MaterialModule,
     NgxMatFileInputModule,
     CargaAsientosRoutingModule,
     SharedModule,
     NewManualModule,
     ReferenciasComplementariasModule,
+    MaterialModule
   ],
   providers: [
     cargaAsientosInterceptorProvider,
