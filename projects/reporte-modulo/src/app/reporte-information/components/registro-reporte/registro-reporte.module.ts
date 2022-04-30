6681b60bf3d89d2e2cc4b3c46351553365a6f3be
luislrz2503 <99489663+luislrz2503@users.noreply.github.com>
2022-04-30 16:45:08 -0500

    feat(main): ajuste para la tabla

--- projects/reporte-modulo/src/app/reporte-information/components/registro-reporte/registro-reporte.module.ts
+++ projects/reporte-modulo/src/app/reporte-information/components/registro-reporte/registro-reporte.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { InputModule } from '../../../shared/component/ui/input/input.module';
 import { ActionsModule } from '../../../shared/component/ui/actions/actions.module';
 import { LoaderModule } from '../../../shared/component/ui/loader/loader.module';
 
-import { TitleHeaderModule } from '../../../shared/component/ui/title-header/title-header.module';
-import { FormErrorsModule } from '../../../shared/component/ui/form-errors/form-errors.module';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
 import { RegistroReporteComponent } from './registro-reporte.component';
 import { MatCardModule } from '@angular/material/card';
-import { SelectModule } from '../../../shared/component/ui/select/select.module';
-import { CalendarModule } from '../../../shared/component/ui/calendar/calendar.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -20,18 +13,10 @@ import { CalendarModule } from '../../../shared/component/ui/calendar/calendar.m
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    InputModule,
-    ActionsModule,
     LoaderModule,
-    TitleHeaderModule,
-    FormErrorsModule,
-    MatExpansionModule,
-    MatIconModule,
+    MatTableModule,
+    ActionsModule,
     MatCardModule,
-    SelectModule,
-    CalendarModule,
   ],
   exports: [RegistroReporteComponent],
 })
