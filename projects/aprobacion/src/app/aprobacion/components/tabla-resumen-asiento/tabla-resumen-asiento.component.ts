450c72bdb6d3614bdf4887c9306651745cf15b2a
luislrz2503 <99489663+luislrz2503@users.noreply.github.com>
2022-05-27 16:32:40 -0500

    feat(main): ajuste pra monto

--- projects/aprobacion/src/app/aprobacion/components/tabla-resumen-asiento/tabla-resumen-asiento.component.ts
+++ projects/aprobacion/src/app/aprobacion/components/tabla-resumen-asiento/tabla-resumen-asiento.component.ts
@@ -9,4 +9,28 @@ import { AccountLine } from '../../models/limite.model';
 export class TablaResumenAsientoComponent {
   @Input() cuentas: AccountLine[] = [];
   displayedColumns: string[] = ['numeracion', 'primerDigito', 'cuenta', 'nombre', 'moneda', 'debito', 'credito', 'neto'];
+
+  numerTranfors(number: any): string {
+    var num = Number(number)?.toFixed(2)
+    var numArr = num.split('.')
+    // eslint-disable-next-line no-redeclare
+    var [num, dotNum] = numArr
+
+
+    var operateNum = num.split('').reverse()
+    var result = [], len = operateNum.length
+    for (var i = 0; i < len; i++) {
+      result.push(operateNum[i])
+      if (((i + 1) % 3 === 0) && (i !== len - 1)) {
+        result.push(',')
+      }
+    }
+
+    if (dotNum) {
+      result.reverse().push('.', ...dotNum)
+      return result.join('')
+    } else {
+      return result.reverse().join('')
+    }
+  }
 }
